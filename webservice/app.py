f39bd831c5333a2799549f63af2b6f2d0c7a2254
Xiangxiang Chen <xiangxiang.chen@fau.de>
2021-04-25 12:49:52 +0200

    add functions of returning list of image urls and delete images

--- webservice/app.py
+++ webservice/app.py
@@ -1,17 +1,15 @@
-from flask import Flask, flash, redirect, render_template, request, url_for, send_from_directory
-#from flask_uploads import UploadSet,configure_uploads,IMAGES,DATA,ALL
+from flask import Flask, flash, redirect, jsonify,render_template, request, url_for, send_from_directory
 import os
 from werkzeug.utils import secure_filename
-from helpers import allowed_file
 
 # set path to store uploaded pics and videos
-UPLOAD_FOLDER = os.path.abspath(os.path.dirname(__file__)) + "/static/"
+UPLOAD_FOLDER = os.path.abspath(os.path.dirname(__file__)) + "/uploaded/"
 # limit type of extensions
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'gif','mp4'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg','gif','mp4'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.secret_key = b'asd_5#y2L"z\n\xec]/'
+app.secret_key = b'wu8QvPtCDIM1/9ceoUS'
 
 @app.route('/')
 def index():
@@ -19,9 +17,8 @@ def index():
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    # POST,
+    # POST
     # get the pics and videos
-    # check if file
     if request.method == 'POST':
         # get uploaded pics/videos list
         files = request.files.getlist("file")
@@ -32,9 +29,6 @@ def upload():
                 filename = secure_filename(file.filename)
                 files_l.append(filename)
                 # save file
-                # folderpath = os.path.join(app.config['UPLOAD_FOLDER'], datetime.now().strftime('%Y%m%d%H%M%S'))
-                # if not os.path.exists(folderpath):
-                #     os.makedirs(folderpath, 755)
                 filepath = os.path.join(UPLOAD_FOLDER,filename)
                 file.save(filepath)
             elif file and not allowed_file(file.filename):
@@ -47,10 +41,39 @@ def upload():
     else:
         return render_template("upload.html")
 
-@app.route('/uploads/<filename>')
+# create url to sent files
+@app.route('/uploaded/<filename>')
 def uploaded_file(filename):
     return send_from_directory(UPLOAD_FOLDER,
                                filename)
+
+#return list of all urls of uploaded files
+@app.route('/all')
+def all():
+    names = os.listdir(app.config['UPLOAD_FOLDER'])
+    urls = []
+    for name in names:
+        urls.append(url_for("uploaded_file",filename = name))
+    return jsonify(urls)
+
+# #delete files
+@app.route('/delete',methods=['GET','POST'])
+def delete():
+    if request.method == 'POST':
+        files = request.form.getlist("files")
+        if files:
+            for file in files:
+                fpath = os.path.join(app.config['UPLOAD_FOLDER'], file)
+                if os.path.exists(fpath):
+                    os.remove(fpath)
+        else:
+            return jsonify("request is empty")
+        return render_template("uploaded.html")
+    else:
+        return render_template("upload.html")
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 if __name__ == '__main__':
     app.run()
