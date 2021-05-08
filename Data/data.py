4a5fa88455de0a85d6f353f79f89a6251ffd1de7
xxchen <xiangxiang.chen@fau.de>
2021-05-08 21:48:57 +0200

    remove content that not qualified to release

--- Data/data.py
+++ Data/data.py
@@ -16,28 +16,3 @@ def storeimage(directory,colorized_data):
     cv2.destroyAllWindows()
     return newpath
 
-def processVideo(path,videoName):
-    videoName = os.path.join(path,videoName)
-    video = cv2.VideoCapture(videoName)
-    if (video.isOpened() == False):
-        print("Error opening video")
-    FPS = 60
-    video.set(cv2.CAP_PROP_FPS, FPS)
-    currentFrame = 0
-    while (video.isOpened()):
-        ret,frame = video.read()
-        if ret == True:
-            folder_name = os.path.join(path,'saved_frames', str(currentFrame) + '.png')
-            print('Creating...' + folder_name)
-            cv2.imshow('Frame', frame)
-            cv2.imwrite(folder_name, frame)
-            currentFrame += 1
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
-        else:
-            break
-    video.release()
-    cv2.destroyAllWindows()
-    return folder_name
-
-
