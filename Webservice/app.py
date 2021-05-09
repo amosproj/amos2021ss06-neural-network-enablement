65c82eb70095f96d6f25cecc8e6468a12a9a4bd9
Wolfgang Meier <wolfgang.meier@fau.de>
2021-05-09 17:04:37 +0200

    Webservice: remove some GET routes

--- Webservice/app.py
+++ Webservice/app.py
@@ -18,32 +18,30 @@ app.secret_key = b'wu8QvPtCDIM1/9ceoUS'
 def index():
     return render_template("index.html")
 
-@app.route('/upload/', methods=['GET', 'POST'])
+
+@app.route('/upload/', methods=['POST'])
 def upload():
-    # POST
     # get the pics and videos
     # TODO change store folder of pictures
-    if request.method == 'POST':
-        # get uploaded pics/videos list
-        # 4/29 by Xiangxiang Chen, remove loop for file list
-        file = request.files.get("file")
-        if file and allowed_file(file.filename):
-            sfilename = secure_filename(file.filename)
-            # add timestamp at the beginning of filename
-            nowTime = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-            filename = str(nowTime) + "_" + sfilename
-            # create folder and save file
-            folderpath = os.path.join(UPLOAD_FOLDER,filename.rsplit('.', 1)[0])
-            os.mkdir(folderpath)
-            filepath = os.path.join(folderpath,filename)
-            file.save(filepath)
-            return jsonify(msg="Upload successfully"), 200
-        elif file and not allowed_file(file.filename):
-            return jsonify(msg ="The file format is not supported"), 400
-        else:
-            return jsonify(msg ="No upload file"), 400
+    # get uploaded pics/videos list
+    # 4/29 by Xiangxiang Chen, remove loop for file list
+    file = request.files.get("file")
+    if file and allowed_file(file.filename):
+        sfilename = secure_filename(file.filename)
+        # add timestamp at the beginning of filename
+        nowTime = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+        filename = str(nowTime) + "_" + sfilename
+        # create folder and save file
+        folderpath = os.path.join(UPLOAD_FOLDER,filename.rsplit('.', 1)[0])
+        os.mkdir(folderpath)
+        filepath = os.path.join(folderpath,filename)
+        file.save(filepath)
+        return jsonify(msg="Upload successfully"), 200
+    elif file and not allowed_file(file.filename):
+        return jsonify(msg ="The file format is not supported"), 400
     else:
-        return render_template("index.html")
+        return jsonify(msg ="No upload file"), 400
+
 
 # create url to sent files
 @app.route('/uploaded/<filename>')
@@ -52,8 +50,9 @@ def uploaded_file(filename):
     return send_from_directory(folderpath,
                                filename)
 
+
 #return list of all urls of uploaded files
-@app.route('/all')
+@app.route('/all/')
 def all():
     urls =[]
     for root,dirs,files in os.walk(app.config['UPLOAD_FOLDER']):
@@ -66,21 +65,18 @@ def all():
 # #delete files
 @app.route('/delete/', methods=['POST'])
 def delete():
-    if request.method == 'POST':
+    filename = request.get_json()['name'] if 'name' in request.get_json() else None
 
-        filename = request.get_json()['name'] if 'name' in request.get_json() else None
-
-        if filename:
-            deletepath = os.path.join(app.config['UPLOAD_FOLDER'],filename.rsplit('.', 1)[0])
-            if os.path.exists(deletepath):
-                shutil.rmtree(deletepath)
-                return jsonify(msg = "Deleted!"), 200
-            else:
-                return jsonify(msg = "Pictures not found!"), 404
+    if filename:
+        deletepath = os.path.join(app.config['UPLOAD_FOLDER'],filename.rsplit('.', 1)[0])
+        if os.path.exists(deletepath):
+            shutil.rmtree(deletepath)
+            return jsonify(msg = "Deleted!"), 200
         else:
-            return jsonify(msg = "request is empty"), 400
+            return jsonify(msg = "Pictures not found!"), 404
     else:
-        return render_template("index.html")
+        return jsonify(msg = "request is empty"), 400
+
 
 # # colorize files
 # @app.route('/colorize',methods=['POST'])
@@ -125,6 +121,7 @@ def delete():
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 if __name__ == '__main__':
     os.environ['FLASK_ENV'] = "development"
