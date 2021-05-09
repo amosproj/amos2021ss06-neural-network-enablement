69110b9f177924bd8000f8329732d0849536dd51
Xiangxiang Chen <xiangxiang.chen@fau.de>
2021-05-09 18:36:48 +0200

    modify the allowed extensions

--- Webservice/app.py
+++ Webservice/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, jsonify,render_template, request, url_for, send_from_directory
-import os
+from flask import Flask, jsonify, render_template, request, url_for, send_from_directory
 from werkzeug.utils import secure_filename
+import os
 import datetime
 import shutil
 #from pipeline import colorize_image
@@ -8,7 +8,7 @@ import shutil
 # set path to store uploaded pics and videos
 UPLOAD_FOLDER = os.path.abspath(os.path.dirname(__file__)) + "/uploaded/"
 # limit type of extensions
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif','mp4', 'mkv', 'webm'}
+ALLOWED_EXTENSIONS = {'pic' : ['png', 'jpg', 'jpeg', 'gif'],'video' : ['mp4', 'mkv', 'webm']}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -22,9 +22,6 @@ def index():
 @app.route('/upload/', methods=['POST'])
 def upload():
     # get the pics and videos
-    # TODO change store folder of pictures
-    # get uploaded pics/videos list
-    # 4/29 by Xiangxiang Chen, remove loop for file list
     file = request.files.get("file")
     if file and allowed_file(file.filename):
         sfilename = secure_filename(file.filename)
@@ -57,7 +54,7 @@ def all():
     urls =[]
     for root,dirs,files in os.walk(app.config['UPLOAD_FOLDER']):
         for file in files:
-            if file.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS:
+            if file.rsplit('.', 1)[1].lower() in (ALLOWED_EXTENSIONS['pic'] or ALLOWED_EXTENSIONS['video']):
                 urls.append(url_for("uploaded_file", filename=file))
     return jsonify(urls)
 
@@ -78,36 +75,38 @@ def delete():
         return jsonify(msg = "request is empty"), 400
 
 
-# # colorize files
-# @app.route('/colorize',methods=['POST'])
+# colorize files
+# @app.route('/colorize/',methods=['POST'])
 # def colorize():
-#     if request.method == 'POST':
-#         # 1 get file list
-#         # 2 loop
-#         # 3 identify type of file, video->colorize_video , img-> colorize_image
-#         filename = request.get_json()['name'] if 'name' in request.get_json() else None
 #
-#         # get the name of file chosen by user
-#         filenames = request.get_json()['name'] if 'name' in request.get_json() else None
-#         # in discussion,so far, use UPLOAD_FOLDER
-#         image_path_input = UPLOAD_FOLDER
-#
-#
-#         # colorize_image() = [data from host to device, preprocess, inference, postprocess,data from device to host]
-#         colorize_image(image_path_input,image_path_output)
-#
+#     filename = request.get_json()['name'] if 'name' in request.get_json() else None
+#     if filename:
+#         fpath = os.path.join(UPLOAD_FOLDER, filename.rsplit('.', 1)[0])
+#         # colorize_image
+#         if filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS['pic']:
+#             colorize_image(fpath, fpath)
+#         # colorize_image
+#         # elif filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS['video']:
+#         #     colorize_video(fpath, fpath)
+#         else:
+#             return jsonify("The format is not supported"),400
+#         # return value need further discussion
 #         return render_template("result.html")
+#     else:
+#         return jsonify("No input gray file"), 400
+
 
 # # if this router is needed then present result on a webpage, if not then just delete
-# @app.route('/result')
+# @app.route('/result/')
 # def result():
-#     #TODO
+#     # TODO
 #     return render_template("result.html")
 
 #return json for all orgin and colorized pics
-# @app.route('/showresults')
+# @app.route('/showresults/')
 # def showresults():
-#     # TODO, an example
+#     # TODO
+#     # an example
 #     # {
 #     #     all: [
 #     #         {original_image: uploaded / image.png,
@@ -120,7 +119,7 @@ def delete():
 #     return jsonify(results)
 
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in (ALLOWED_EXTENSIONS['pic'] or ALLOWED_EXTENSIONS['video'])
 
 
 if __name__ == '__main__':
