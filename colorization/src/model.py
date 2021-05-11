3e25147ef7181ab970611a569de7d2fef9b14647
rajkumarkolan <kolanrajkumar09@gmail.com>
2021-05-11 23:19:42 +0200

    Implementation of model.

--- colorization/src/model.py
+++ colorization/src/model.py
@@ -1,1 +1,9 @@
-import caffe
+import cv2
+
+def model_process(modelPath):
+    model_file = modelPath + 'colorization.prototxt'
+    pretrained_network = modelPath + 'colorization.caffemodel'
+    caffe_net = cv2.dnn.readNetFromCaffe(model_file,pretrained_network)
+    caffe_net.set_mode_cpu()
+    caffe_net.setInput(cv2.dnn.blobFromImage(l_channel))
+    ab_channel = caffe_net.forward()[0,:,:,:].transpose((1,2,0))
