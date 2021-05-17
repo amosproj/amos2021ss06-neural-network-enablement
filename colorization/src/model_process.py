0dc161a2950cefc7ae1ba0f2becd4da80486433a
ce52supi <susmi4356@gmail.com>
2021-05-17 16:53:02 +0200

    model_process: formatting the code.

--- colorization/src/model_process.py
+++ colorization/src/model_process.py
@@ -1,17 +1,20 @@
 import logging
 import acl
 
+
 class Modelprocess:
-    def __init__(self,loadflag=False,modelId=0,modelMemPtr=None,modelMemSize=0,modelWeightPtr=None,modelWeightSize=0,modelDesc=None,input=None,output=None):
+    def __init__(self, loadflag=False, modelId=0, modelMemPtr=None, modelMemSize=0, modelWeightPtr=None,
+                 modelWeightSize=0, modelDesc=None, input=None, output=None):
         self.loadflag = loadflag
-        self.modelId = modelId             # int, ID of the model to be inferred.
+        self.modelId = modelId  # int, ID of the model to be inferred.
         self.modelMemPtr = modelMemPtr
         self.modelMemSize = modelMemSize  # int, model data length, in bytes
-        self.modelWeightPtr = modelWeightPtr   # int, pointer of the model weight memory (for storing weight data) on the device. The pointer is managed by users.
-        self.modelWeightSize = modelWeightSize   # int, size of the weight memory required by the model, in bytes
-        self.modelDesc = modelDesc # Description of the model.
+        self.modelWeightPtr = modelWeightPtr  # int, pointer of the model weight memory (for storing weight data) on
+        # the device. The pointer is managed by users.
+        self.modelWeightSize = modelWeightSize  # int, size of the weight memory required by the model, in bytes
+        self.modelDesc = modelDesc  # Description of the model.
         self.input = input  # int, pointer object of the input data of model inference
-        self.output = output    # int, pointer object of the output data of model inference.
+        self.output = output  # int, pointer object of the output data of model inference.
 
     def __del__(self):  # Calling destructor
         self.Unload()
@@ -19,7 +22,7 @@ class Modelprocess:
         self.DestroyInput()
         self.DestroyOutput()
 
-    def LoadModelFromFileWithMem(self,modelPath):
+    def LoadModelFromFileWithMem(self, modelPath):
 
         """ Function usage: Loads offline model data (adapted to the Ascend AI processor) from a file.
         The offline model file is the *.om file that adapts to the Ascend AI processor
@@ -32,19 +35,20 @@ class Modelprocess:
         if self.loadflag:
             logging.error("has already loaded a model")
             return 1
-        work_size, weight_size, ret = acl.mdl.query_size(modelPath,self.modelMemSize,self.modelWeightSize)
+        work_size, weight_size, ret = acl.mdl.query_size(modelPath, self.modelMemSize, self.modelWeightSize)
         if ret != acl.ACL_ERROR_None:
             logging.error("query model failed, model file is %s", modelPath)
             return 1
-        ret = acl.rt.malloc(self.modelMemPtr,self.modelMemSize,acl.ACL_MEM_MALLOC_HUGE_FIRST)
+        ret = acl.rt.malloc(self.modelMemPtr, self.modelMemSize, acl.ACL_MEM_MALLOC_HUGE_FIRST)
         if ret != acl.ACL_ERROR_NONE:
-            logging.error("malloc buffer for mem failed, require size is %zu",self.modelMemSize)
+            logging.error("malloc buffer for mem failed, require size is %zu", self.modelMemSize)
             return 1
-        ret = acl.rt.malloc(self.modelWeightPtr,self.modelWeightSize,acl.ACL_MEM_MALLOC_HUGE_FIRST)
+        ret = acl.rt.malloc(self.modelWeightPtr, self.modelWeightSize, acl.ACL_MEM_MALLOC_HUGE_FIRST)
         if ret != acl.ACL_ERROR_NONE:
             logging.error("malloc buffer for weight failed, require size is %zu", self.modelWeightSize)
             return 1
-        model_id, ret = acl.mdl.load_from_file_with_mem(modelPath, self.modelId, self.modelMemPtr, self.modelMemSize,self.modelWeightPtr,self.modelWeightSize)
+        model_id, ret = acl.mdl.load_from_file_with_mem(modelPath, self.modelId, self.modelMemPtr, self.modelMemSize,
+                                                        self.modelWeightPtr, self.modelWeightSize)
         if ret != acl.ACL_ERROR_NONE:
             logging.error("load model from file failed, model file is %s", modelPath)
             return 1
@@ -57,7 +61,7 @@ class Modelprocess:
         if self.modelDesc is None:
             logging.error("create model description failed")
             return 1
-        ret = acl.mdl.get_desc(self.modelDesc,self.modelId)
+        ret = acl.mdl.get_desc(self.modelDesc, self.modelId)
         if ret is not None:
             logging.error("get model description failed")
             return 1
@@ -69,16 +73,16 @@ class Modelprocess:
             acl.mdl.destroy_desc(self.modelDesc)
             self.modelDesc = None
 
-    def CreateInput(self,inputDataBuffer,bufferSize):
+    def CreateInput(self, inputDataBuffer, bufferSize):
         self.input = acl.mdl.create_dataset()
         if self.input is None:
             logging.error("can't create data buffer, create input failed")
             return 1
-        inputData = acl.create_data_buffer(inputDataBuffer,bufferSize)
+        inputData = acl.create_data_buffer(inputDataBuffer, bufferSize)
         if inputData is None:
             logging.error("can't create data buffer, create input failed")
             return 1
-        dataset,ret = acl.mdl.add_dataset_buffer(self.input,inputData)
+        dataset, ret = acl.mdl.add_dataset_buffer(self.input, inputData)
         if inputData is None:
             logging.error("can't add data buffer, create input failed")
             acl.destroy_data_buffer(inputData)
@@ -89,8 +93,8 @@ class Modelprocess:
     def DestroyInput(self):
         if self.input is None:
             return
-        for i in range(0,acl.mdl.get_dataset_num_buffers(self.input)):
-            dataBuffer = acl.mdl.get_dataset_buffer(self.input,i)
+        for i in range(0, acl.mdl.get_dataset_num_buffers(self.input)):
+            dataBuffer = acl.mdl.get_dataset_buffer(self.input, i)
             acl.destroy_data_buffer(dataBuffer)
         acl.mdl.destroy_dataset(self.input)
         self.input = None
@@ -104,19 +108,19 @@ class Modelprocess:
             logging.error("cannot create dataset,create output failed")
             return 1
         outputSize = acl.mdl.get_num_outputs(self.modelDesc)
-        for i in range(0,outputSize):
-            buffer_size = acl.mdl.get_output_size_by_index(self.modelDesc,i)
+        for i in range(0, outputSize):
+            buffer_size = acl.mdl.get_output_size_by_index(self.modelDesc, i)
             outputBuffer = None
-            ret = acl.rt.malloc(outputBuffer,buffer_size,acl.ACL_MEM_MALLOC_NORMAL_ONLY)
+            ret = acl.rt.malloc(outputBuffer, buffer_size, acl.ACL_MEM_MALLOC_NORMAL_ONLY)
             if ret != acl.ACL_ERROR_NONE:
-                logging.error("can't malloc buffer, size is %zu, create output failed",buffer_size)
+                logging.error("can't malloc buffer, size is %zu, create output failed", buffer_size)
                 return 1
-            outputData = acl.create_data_buffer(outputBuffer,buffer_size)
+            outputData = acl.create_data_buffer(outputBuffer, buffer_size)
             if ret != acl.ACL_ERROR_NONE:
                 logging.error("can't create data buffer, create output failed")
                 acl.rt.free(outputBuffer)
                 return 1
-            ret = acl.mdl.add_dataset_buffer(self.output,outputData)
+            ret = acl.mdl.add_dataset_buffer(self.output, outputData)
             if ret != acl.ACL_ERROR_NONE:
                 logging.error("can't add data buffer, create output failed")
                 acl.rt.free(outputBuffer)
@@ -128,8 +132,8 @@ class Modelprocess:
     def DestroyOutput(self):
         if self.output is None:
             return
-        for i in range(0,acl.mdl.get_dataset_num_buffers(self.output)):
-            dataBuffer = acl.mdl.get_dataset_buffer(self.output,i)
+        for i in range(0, acl.mdl.get_dataset_num_buffers(self.output)):
+            dataBuffer = acl.mdl.get_dataset_buffer(self.output, i)
             data = acl.get_data_buffer_addr(dataBuffer)
             acl.rt.free(data)
             acl.destroy_data_buffer(dataBuffer)
@@ -170,9 +174,3 @@ class Modelprocess:
 
     def GetModelOutputData(self):
         return self.output
-
-
-
-
-
-
