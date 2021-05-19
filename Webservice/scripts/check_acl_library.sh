584d6437e385195923854e99847cf3e3887283eb
Wolfgang Meier <wolfgang.meier@fau.de>
2021-05-19 18:15:58 +0200

    Move env_test script to Webservice scripts folder

--- Webservice/scripts/check_acl_library.sh
+++ Webservice/scripts/check_acl_library.sh
@@ -3,13 +3,14 @@
 export PYTHONPATH=$PYTHONPATH:$HOME/Ascend/ascend-toolkit/latest/pyACL/python/site-packages/acl
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/Ascend/ascend-toolkit/latest/acllib_centos7.6.x86_64/acllib/lib64
-
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/Ascend/ascend-toolkit/latest/x86_64-linux_gcc7.3.0/atc/lib64
-
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/Ascend/ascend-toolkit/latest/toolkit/lib64
 
 python3 -c "import acl"
 
 if [ $? -eq 0 ]; then
     echo "Success"
+    exit 0
+else
+    exit 1
 fi
