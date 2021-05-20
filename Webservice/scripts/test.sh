f79bafafda83f1b286795b642d973fc03479db83
Wolfgang Meier <wolfgang.meier@fau.de>
2021-05-20 13:35:58 +0200

    Remove duplicated script

--- Webservice/scripts/test.sh
+++ Webservice/scripts/test.sh
@@ -5,27 +5,5 @@
 
 cd `dirname "$0"`/../../
 
-
 # Run the tests
 Webservice/venv/bin/pytest colorization/src/tests.py
-TEST_RESULT=$?
-
-echo
-echo "========================= Checking the code formatting ========================="
-echo
-
-# Check the code formatting
-Webservice/venv/bin/flake8 . --exclude Webservice/venv/
-CODEFORMAT_RESULT=$?
-
-if [ $CODEFORMAT_RESULT -ne 0 ]; then
-    echo
-    echo "Code formatting check failed."
-    exit 1
-else
-    echo
-    echo "Code formatting check succeeded."
-fi
-
-exit $TEST_RESULT
-
