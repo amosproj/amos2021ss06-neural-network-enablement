68c5ce1531e4b4a87bab4ea3d1aa96e399229050
Wolfgang Meier <wolfgang.meier@posteo.de>
2021-05-24 18:38:16 +0200

    Formatting

--- Webservice/test_web.py
+++ Webservice/test_web.py
@@ -9,10 +9,6 @@ UPLOAD_FOLDER = os.path.abspath(os.path.dirname(__file__)) + "/uploaded/"
 
 class BasicTests(unittest.TestCase):
 
-    ############################
-    #### setup and teardown ####
-    ############################
-
     # executed prior to each test
     def setUp(self):
         app.config['TESTING'] = True
@@ -21,24 +17,24 @@ class BasicTests(unittest.TestCase):
         app.secret_key = b'wu8QvPtCDIM1/9ceoUS'
         self.app = app.test_client()
 
+
     # executed after each test
     def tearDown(self):
         # check the test data would not influence the Webservice
         pass
 
-    ###############
-    #### tests ####
-    ###############
 
     def test_main_page(self):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
+
 
     # TODO: upload() + all(), upload image
     def test_upload_image(self):
         # check return code and msg, all conditions
         # check return all urls of pictures
         pass
+
 
     # TODO: result() + colorize()
     def test_colorized_result(self):
@@ -47,9 +43,9 @@ class BasicTests(unittest.TestCase):
         # and result['origin'],result['colorized']
         pass
 
+
     # TODO: delete() + all(), similar to test_upload_image()
     def test_delete(self):
         # check return code and msg
         # check files&folders is not there anymore
         pass
-
