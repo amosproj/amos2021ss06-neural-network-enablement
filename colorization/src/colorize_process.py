aa307fc31ab1b3a728c1d25d0ce498bed08d9238
ce52supi <susmi4356@gmail.com>
2021-06-02 00:47:27 +0200

    checking postprocess.

--- colorization/src/colorize_process.py
+++ colorization/src/colorize_process.py
@@ -321,7 +321,7 @@ class ColorizeProcess:
         inference_result = numpy.reshape(inference_result, (int(self.modelWidth / 2),
                                                             int(self.modelHeight / 2), 2))
         a_channel, b_channel = cv2.split(inference_result)
-
+        print(type(a_channel))
         # pull out L channel in original/source image
         input_image = cv2.imread(input_image_path, cv2.IMREAD_COLOR)
         input_image = numpy.float32(input_image)
@@ -331,8 +331,9 @@ class ColorizeProcess:
         print(L.shape)
 
         # resize to match the size of original image L
-        rows = input_image.shape[0]
-        cols = input_image.shape[1]
+        rows = numpy.float32(input_image.shape[0])
+        print(type(rows))
+        cols = numpy.float32(input_image.shape[1])
         a_channel_resize = cv2.resize(a_channel, (rows, cols))
         b_channel_resize = cv2.resize(b_channel, (rows, cols))
 
