fb51d3c620f1741d5bc3c8b72869e861e5111bb0
Wolfgang Meier <wolfgang.meier@fau.de>
2021-06-03 14:17:25 +0200

    Show print statements in pytest log (#112)

--- Webservice/scripts/test.sh
+++ Webservice/scripts/test.sh
@@ -8,12 +8,12 @@ cd `dirname "$0"`/../../
 # Run the tests
 
 # for the webservice
-Webservice/venv/bin/pytest Webservice/test_web.py --cov=. --cov-config=.coveragerc --cov-report=''
+Webservice/venv/bin/pytest Webservice/test_web.py --cov=. --cov-config=.coveragerc --cov-report='' --capture=tee-sys
 
 # for each pipeline unittest
-Webservice/venv/bin/pytest colorization/src/tests.py -k 'step_preprocess' --cov=. --cov-config=.coveragerc --cov-append --cov-report=''
-Webservice/venv/bin/pytest colorization/src/tests.py -k 'step_colorize' --cov=. --cov-config=.coveragerc --cov-append --cov-report=''
-Webservice/venv/bin/pytest colorization/src/tests.py -k 'step_postprocess' --cov=. --cov-config=.coveragerc --cov-append --cov-report=''
+Webservice/venv/bin/pytest colorization/src/tests.py -k 'step_preprocess'  --cov=. --cov-config=.coveragerc --cov-append --cov-report='' --capture=tee-sys
+Webservice/venv/bin/pytest colorization/src/tests.py -k 'step_colorize'    --cov=. --cov-config=.coveragerc --cov-append --cov-report='' --capture=tee-sys
+Webservice/venv/bin/pytest colorization/src/tests.py -k 'step_postprocess' --cov=. --cov-config=.coveragerc --cov-append --cov-report='' --capture=tee-sys
 
 # for the pipeline functional tests
-Webservice/venv/bin/pytest colorization/src/tests.py -k 'FunctionalTest' --cov=. --cov-config=.coveragerc --cov-append
+Webservice/venv/bin/pytest colorization/src/tests.py -k 'FunctionalTest'   --cov=. --cov-config=.coveragerc --cov-append --capture=tee-sys
