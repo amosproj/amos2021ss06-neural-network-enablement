e6243d3a669587667187795a7c712a744ebbd15a
Emma-An94 <787434822@qq.com>
2021-06-10 23:47:58 +0200

    fix path bug

--- colorization/src/pipeline.py
+++ colorization/src/pipeline.py
@@ -77,7 +77,8 @@ def colorize_image(image_path_input, image_path_output):
         return FAILED
 
     #  inference & colorize
-    tmpdir = tempfile.TemporaryDirectory(suffix="_npy", prefix="tp_inference_", dir="/tmp")
+    tmpdir = tempfile.TemporaryDirectory(suffix="_npy", prefix="tp_inference_",
+                                         dir="/tmp")
     inference_output_path = tmpdir.name + '/inference_output.npy'
     ret = colorize.inference(inference_output_path)
     if ret == FAILED:
