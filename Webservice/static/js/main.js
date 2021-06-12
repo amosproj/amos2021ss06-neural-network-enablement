062c53475d64dd4e2c1544fd70c81f2f9f2ab775
Xiangxiang Chen <xiangxiang.chen@fau.de>
2021-06-12 13:57:05 +0200

    dropbox default invisible

--- Webservice/static/js/main.js
+++ Webservice/static/js/main.js
@@ -1,225 +1,218 @@
 // helpers
 
-function showWarningToast(headline, message) {
-    let node = document.createElement('div');
-    node.innerHTML = document.querySelector('#template-toast-warn').innerHTML;
+function showWarningToast (headline, message) {
+  let node = document.createElement('div')
+  node.innerHTML = document.querySelector('#template-toast-warn').innerHTML
 
-    node.querySelector('#toast-headline').textContent = headline;
-    node.querySelector('#toast-message').textContent = message;
+  node.querySelector('#toast-headline').textContent = headline
+  node.querySelector('#toast-message').textContent = message
 
-    Toastify({
-        node: node,
-        duration: 5000,
-        close: true,
-        gravity: "top", // `top` or `bottom`
-        position: "right", // `left`, `center` or `right`
-        stopOnFocus: true, // Prevents dismissing of toast on hover
-        backgroundColor: '#FDE68A', // bg-yellow-200 TODO: use style attribute
-    }).showToast();
+  Toastify({
+    node: node,
+    duration: 5000,
+    close: true,
+    gravity: 'top', // `top` or `bottom`
+    position: 'right', // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    backgroundColor: '#FDE68A', // bg-yellow-200 TODO: use style attribute
+  }).showToast()
 }
-
 
 // -------------------------------------------------------------------------
 
 // dropzone initialization stuff
 
 let config = {
-    url: "/upload/",
-    disablePreviews: true,
-    //  acceptedFiles: ".jpeg,.jpg,.png,.gif,.mp4,.mkv,.webm"
-};
-
-let myDropzone1 = new Dropzone("#upload-button", config);
-let myDropzone2 = new Dropzone("#upload-button-img", config);
-let myDropzone3 = new Dropzone("#upload-button-text", config);
-
-myDropzone1.on("success", successHandler);
-myDropzone2.on("success", successHandler);
-myDropzone3.on("success", successHandler);
-
-myDropzone1.on("error", errorHandler);
-myDropzone2.on("error", errorHandler);
-myDropzone3.on("error", errorHandler);
-
-// success handler: called, after dropzone library successfully uploaded a file
-function successHandler(file, resp) {
-    console.log('success!')
-    //console.log(file)
-    //console.log(resp)
-
-    window.location.reload(true);
+  url: '/upload/',
+  disablePreviews: true,
+  //  acceptedFiles: ".jpeg,.jpg,.png,.gif,.mp4,.mkv,.webm"
 }
 
+let myDropzone1 = new Dropzone('#upload-button', config)
+let myDropzone2 = new Dropzone('#upload-button-img', config)
+let myDropzone3 = new Dropzone('#upload-button-text', config)
+
+myDropzone1.on('success', successHandler)
+myDropzone2.on('success', successHandler)
+myDropzone3.on('success', successHandler)
+
+myDropzone1.on('error', errorHandler)
+myDropzone2.on('error', errorHandler)
+myDropzone3.on('error', errorHandler)
+
+// success handler: called, after dropzone library successfully uploaded a file
+function successHandler (file, resp) {
+  console.log('success!')
+  //console.log(file)
+  //console.log(resp)
+
+  window.location.reload(true)
+}
 
 // error handler: called, after dropzone library fails to upload a file
-function errorHandler(file, error, xhr) {
-    console.log('error!')
+function errorHandler (file, error, xhr) {
+  console.log('error!')
 
-    let message = 'Please try again.'
+  let message = 'Please try again.'
 
-    if (typeof (error) === 'string') {
+  if (typeof (error) === 'string') {
 
-        if (error === 'Server responded with 0 code.') {
-            // Improve default error message by dropzone
-            message = "Couldn't connect to localhost, is the webservice still running?"
-        } else {
-            message = error;
-        }
-
+    if (error === 'Server responded with 0 code.') {
+      // Improve default error message by dropzone
+      message = 'Couldn\'t connect to localhost, is the webservice still running?'
     } else {
-        // print response by api
-        message = error['msg']
-
-        showWarningToast('File upload failed', message);
+      message = error
     }
+
+  } else {
+    // print response by api
+    message = error['msg']
+
+    showWarningToast('File upload failed', message)
+  }
 }
 
 // end dropzone stuff
 // ---------------------------------------------------------------------------------------
 
-
 // sends the service a request to delete the image
 // after the user clicked on the 'delete icon'
-function deleteImage(imgName) {
-    console.log('deleteImage ' + imgName);
+function deleteImage (imgName) {
+  console.log('deleteImage ' + imgName)
 
-    $.ajax({
-        type: "POST",
-        url: "/delete/",
-        data: JSON.stringify({
-            'name': imgName
-        }),
-        success: function () {
-            console.log('success');
+  $.ajax({
+    type: 'POST',
+    url: '/delete/',
+    data: JSON.stringify({
+      'name': imgName
+    }),
+    success: function () {
+      console.log('success')
 
-            function reload() {
-                window.location.reload(true);
-            }
+      function reload () {
+        window.location.reload(true)
+      }
 
-            setTimeout(reload, 500);
-        },
-        error: function () {
-            showWarningToast("Deleting image failed.", "Couldn't connect to server. Is the service running?");
-        },
-        dataType: 'json',
-        contentType: 'application/json',
-    });
+      setTimeout(reload, 500)
+    },
+    error: function () {
+      showWarningToast('Deleting image failed.', 'Couldn\'t connect to server. Is the service running?')
+    },
+    dataType: 'json',
+    contentType: 'application/json',
+  })
 }
-
 
 // sends the service a request to colorize the image
 // after the user clicked on the 'colorize icon'
-function colorizeImage(imgName) {
-    console.log(imgName, 'colorize')
+function colorizeImage (imgName) {
+  console.log(imgName, 'colorize')
 
-    // call colorize function
-    $.ajax({
-        type: "POST",
-        url: "/colorize/",
-        data: JSON.stringify({
-            'name': imgName
-        }),
-        success: function () {
-            console.log('colorization success');
-        },
-        error: function (error) {
-            console.log(error);
-            showWarningToast("Colorizing image failed.", "Couldn't connect to server. Is the service running?");
-            return 1;
-        },
-        dataType: 'json',
-        contentType: 'application/json',
-    });
+  // call colorize function
+  $.ajax({
+    type: 'POST',
+    url: '/colorize/',
+    data: JSON.stringify({
+      'name': imgName
+    }),
+    success: function () {
+      console.log('colorization success')
+    },
+    error: function (error) {
+      console.log(error)
+      showWarningToast('Colorizing image failed.', 'Couldn\'t connect to server. Is the service running?')
+      return 1
+    },
+    dataType: 'json',
+    contentType: 'application/json',
+  })
 
-    return 0;
+  return 0
 }
-
 
 // displays the original and the colorized image next to each other
-function showResult(imgName) {
-    console.log('showResult', imgName);
+function showResult (imgName) {
+  console.log('showResult', imgName)
 
-    // TODO: This should really be a GET request
-    $.ajax({
-        type: "POST",
-        url: "/result/",
-        data: JSON.stringify({
-            'name': imgName
-        }),
-        success: function (response) {
-            console.log('colorization success');
-            // console.log(response);
+  // TODO: This should really be a GET request
+  $.ajax({
+    type: 'POST',
+    url: '/result/',
+    data: JSON.stringify({
+      'name': imgName
+    }),
+    success: function (response) {
+      console.log('colorization success')
+      // console.log(response);
 
-            let original = response['origin'];
-            let colorized = response['colorized'];
+      let original = response['origin']
+      let colorized = response['colorized']
 
-            if (original.includes(imgName)) {
-                console.log(colorized);
+      if (original.includes(imgName)) {
+        console.log(colorized)
 
-                // show result page popup
+        // show result page popup
 
+        document.querySelector('#result-image-original').setAttribute('src', original)
+        document.querySelector('#result-image-colorized').setAttribute('src', colorized)
 
-                document.querySelector('#result-image-original').setAttribute('src', original);
-                document.querySelector('#result-image-colorized').setAttribute('src', colorized);
-
-                setTimeout(function () {
-                    document.querySelector('#result-colorize').classList.remove('invisible');
-                }, 100);
-            }
-        },
-        error: function (error) {
-            console.log(error);
-            showWarningToast("Loading the result failed.", "Couldn't connect to server. Is the service running?");
-            return 1;
-        },
-        dataType: 'json',
-        contentType: 'application/json',
-    });
+        setTimeout(function () {
+          document.querySelector('#result-colorize').classList.remove('invisible')
+        }, 100)
+      }
+    },
+    error: function (error) {
+      console.log(error)
+      showWarningToast('Loading the result failed.', 'Couldn\'t connect to server. Is the service running?')
+      return 1
+    },
+    dataType: 'json',
+    contentType: 'application/json',
+  })
 }
-
 
 // ---------------------------------------------------------------------------------------
 // This is called on every page load.
 //
 // load images and display them in gallery
 $.get('/all/', null, function (data) {
-    if (data.length == 0) {
-        document.getElementById('drpzn').style.display = 'none';
-    } else {
-        data.sort().forEach(function (url) {
-            console.log(url);
+  if (data.length > 0) {
+    document.getElementById('drpzn').classList.remove('invisible')
 
-            let div = document.createElement('div');
-            div.innerHTML = document.querySelector('#template-gallery-image').innerHTML
-            console.log(div)
+    data.sort().forEach(function (url) {
+      console.log(url)
 
-            div.querySelector('#gallery-image').setAttribute('src', url);
+      let div = document.createElement('div')
+      div.innerHTML = document.querySelector('#template-gallery-image').innerHTML
+      console.log(div)
 
-            let imgNameParts = url.split('/')
-            let imgName = imgNameParts[imgNameParts.length - 1]
+      div.querySelector('#gallery-image').setAttribute('src', url)
 
-            let imgButton = div.querySelector('#delete-image-button');
+      let imgNameParts = url.split('/')
+      let imgName = imgNameParts[imgNameParts.length - 1]
 
-            imgButton.onclick = function () {
-                // only allow one mouseclick, remove the eventlistener after first click
-                imgButton.onclick = function () {
-                }
+      let imgButton = div.querySelector('#delete-image-button')
 
-                deleteImage(imgName);
-            }
+      imgButton.onclick = function () {
+        // only allow one mouseclick, remove the eventlistener after first click
+        imgButton.onclick = function () {
+        }
 
-            let colorizeButton = div.querySelector('#colorize-image-button');
-            colorizeButton.onclick = function () {
+        deleteImage(imgName)
+      }
 
-                let res = colorizeImage(imgName);
+      let colorizeButton = div.querySelector('#colorize-image-button')
+      colorizeButton.onclick = function () {
 
-                // success
-                if (res == 0) {
-                    showResult(imgName);
-                }
-            }
+        let res = colorizeImage(imgName)
 
-            document.getElementById("drpzn").appendChild(div)
-        })
-    }
-});
+        // success
+        if (res == 0) {
+          showResult(imgName)
+        }
+      }
+
+      document.getElementById('drpzn').appendChild(div)
+    })
+  }
+})
