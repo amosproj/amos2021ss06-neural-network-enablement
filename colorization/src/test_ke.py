d3f768badcbbb5174edd36da58ca63adfb269fa0
ke <yilinke1995@gmail.com>
2021-06-12 22:37:20 +0200

    Emma, pls run the test_ke to creat the folder and 300 pics

--- colorization/src/test_ke.py
+++ colorization/src/test_ke.py
@@ -1,7 +1,11 @@
-import cv2
 import os
 import videodata
 
+# split the video
+cwd = os.path.abspath(os.path.dirname(__file__))
+video_input_path = os.path.join(cwd, 'test_data/greyscaleVideo.mp4')
+image_output_folder_path = os.path.join(cwd, 'test_data/split_frames')
+videodata.video2frames(video_input_path, image_output_folder_path)
 
 # merge the video
 cwd = os.path.abspath(os.path.dirname(__file__))
