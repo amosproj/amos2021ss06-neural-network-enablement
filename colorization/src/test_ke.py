386637ad9d651aa33175dd6793cdc3e484b468d8
ke <yilinke1995@gmail.com>
2021-06-13 13:10:27 +0200

    changed a bit in frame2video and video2frame add sm documentation wrote the test

--- colorization/src/test_ke.py
+++ colorization/src/test_ke.py
@@ -3,12 +3,15 @@ import videodata
 
 # split the video
 cwd = os.path.abspath(os.path.dirname(__file__))
-video_input_path = os.path.join(cwd, 'test_data/greyscaleVideo.mp4')
+video_input_path = os.path.join(cwd, 'test_data/input_image_2.jpg')
 image_output_folder_path = os.path.join(cwd, 'test_data/split_frames')
+#os.mkdir(image_output_folder_path)
 videodata.video2frames(video_input_path, image_output_folder_path)
+
 
 # merge the video
 cwd = os.path.abspath(os.path.dirname(__file__))
 image_output_folder_path = os.path.join(cwd, 'test_data/split_frames')
 video_output_path = os.path.join(cwd, 'test_data/merged_video')
+#os.mkdir(video_output_path)
 videodata.frames2video(image_output_folder_path, video_output_path)
--- colorization/src/videodata.py
+++ colorization/src/videodata.py
@@ -2,6 +2,7 @@ import cv2
 import os
 import acl_constants
 import numpy
+import shutil
 def video2frames(video_input_path, image_output_folder_path):
     """This function is used to convert video into images.
      Args:
@@ -12,12 +13,13 @@ def video2frames(video_input_path, image_output_folder_path):
         0 on success.
      """
     video = cv2.VideoCapture(video_input_path)
-    if (video.isOpened() is False):
+    type = os.path.splitext(video_input_path)[-1]
+    if (video.isOpened() is False) or (not (type == '.mp4')):
         print("Error opening video")
         return acl_constants.FAILED
     FPS = 60  # frames per second
     video.set(cv2.CAP_PROP_FPS, FPS)
-    currentFrame = 000
+    currentFrame = 0
     while (video.isOpened()):
         ret, frame = video.read()
         if ret is True:
@@ -35,10 +37,6 @@ def video2frames(video_input_path, image_output_folder_path):
     cv2.destroyAllWindows()
     return acl_constants.SUCCESS
 
-# frames2video function :
-# (image_input_folder_path, video_output_path)
-# input path is a folder, output path includes the filename
-
 
 def frames2video(image_input_folder_path, video_output_path):
     '''This function is used to convert images into a video.
@@ -46,7 +44,8 @@ def frames2video(image_input_folder_path, video_output_path):
         image_input_folder_path: path to the split images.
         video_output_path: path to the merged video
     Returns:
-        0 for success.
+        SUCCESS for fail.
+        FAILED for success.
     '''
     mat = cv2.imread(os.path.join(image_input_folder_path + '/0.png'),
                      cv2.IMREAD_COLOR)
@@ -54,17 +53,20 @@ def frames2video(image_input_folder_path, video_output_path):
     if numpy.any(mat) is None:
         return acl_constants.FAILED
     size = mat.shape[:2]
-    filelist = os.listdir(image_input_folder_path)
     FPS = 60
     fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
     video = cv2.VideoWriter(os.path.join(video_output_path, "out01.avi"),
                             fourcc, FPS, (size[1], size[0]))
-
-    for items in filelist:
-        print(items)
-        if items.endswith('.png'):
-            item = image_input_folder_path + '/' + items
-            img = cv2.imread(item)
-            video.write(img)
+    files = os.listdir(image_input_folder_path)
+    length = len(files)
+    for i in range(0, length):
+        index = str(i)
+        item = image_input_folder_path + '/' + index + '.png'
+        print(item)
+        img = cv2.imread(item)
+        video.write(img)
     video.release()
+    # after merged video delate the image_input_folder_path folder
+    shutil.rmtree(image_input_folder_path)
     return acl_constants.SUCCESS
+
