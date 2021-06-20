9f18fcd22a077144a217ad2bb0039deac99f8f87
ce52supi <susmi4356@gmail.com>
2021-06-20 21:52:08 +0200

    remove preprocess.

--- colorization/model_processor.py
+++ colorization/model_processor.py
@@ -1,15 +1,7 @@
 import os
-import cv2
-import numpy as np
-import argparse
 import sys
-
 sys.path.append('../')
-
 from acl_model import Model
-
-heatmap_width = 92
-heatmap_height = 92
 
 
 class ModelProcessor:
@@ -26,51 +18,12 @@ class ModelProcessor:
         # load model from path, and get model ready for inference
         self.model = Model(acl_resource, params['model_dir'])
 
-    def predict(self, img_original):
-        
-        #preprocess image to get 'model_input'
-        model_input = self.preprocess(img_original)
-
+    def predict(self, model_input):
         # execute model inference
         result = self.model.execute([model_input])[0]
-#        np.save('test.npy', result)
-        
         canvas = result[0]
         print(result[0].shape)
-
         print(result.dtype)
-
-        # postprocessing: use the heatmaps (the second output of model) to get the joins and limbs for human body
-        # Note: the model has multiple outputs, here we used a simplified method, which only uses heatmap for body joints
-        #       and the heatmap has shape of [1,14], each value correspond to the position of one of the 14 joints. 
-        #       The value is the index in the 92*92 heatmap (flatten to one dimension)
-        # calculate the scale of original image over heatmap, Note: image_original.shape[0] is height
-
         return canvas
-
-    def preprocess(self,img_original):
-        '''
-        preprocessing: resize image to model required size, and normalize value between [0,1]
-        '''
-
-        mat = img_original
-
-        if np.any(mat) is None:  # if matrix is empty, every term is none
-            return FAILED
-
-        mat = mat.astype(np.float32)
-
-        reiszeMat = cv2.resize(mat, (self._model_width, self._model_height))
-
-        # deal image
-        reiszeMat = 1.0 * reiszeMat / 255
-        reiszeMat = cv2.cvtColor(reiszeMat, cv2.COLOR_BGR2Lab)
-
-        # pull out L channel and subtract 50 for mean-centering
-        # channel[0] = L, [1] = A, [2] = B
-        channels = cv2.split(reiszeMat)
-        
-        print(f'{len(channels)} channels')
-        return channels[0] - 50
 
 
