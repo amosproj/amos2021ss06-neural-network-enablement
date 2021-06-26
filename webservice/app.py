abb8ac75f3507433e88357ea2b4c51ffbd51b187
Wolfgang Meier <wolfgang.meier@fau.de>
2021-06-26 22:00:08 +0200

    Revert "comment out colorization"
    This reverts commit 0a8b5b3edac0c5dc7c772e20af587b5c54395e6b.

--- webservice/app.py
+++ webservice/app.py
@@ -3,6 +3,7 @@ from werkzeug.utils import secure_filename
 import os
 import datetime
 import shutil
+import colorization.pipeline as pipeline
 import cv2
 
 # from colorization.src.pipeline import *
@@ -207,7 +208,7 @@ def colorize():
         # colorize_image
         if extension.lower() in ALLOWED_EXTENSIONS['pic']:
             # temporarily use if True
-            if True: #pipeline.colorize_image(finpath, foutpath) == 0:
+            if pipeline.colorize_image(finpath, foutpath) == 0:
                 # return page need further discussion
                 return jsonify(msg="Colorization successful."), 200
             else:
