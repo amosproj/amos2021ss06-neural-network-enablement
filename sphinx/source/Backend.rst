f457e8c2b9d400f9e01145e30fc3e67697da04d2
ce52supi <susmi4356@gmail.com>
2021-07-09 18:53:46 +0200

    update documentation.

--- sphinx/source/Backend.rst
+++ sphinx/source/Backend.rst
@@ -4,11 +4,4 @@ Backend colorization
 .. toctree::
    :maxdepth: 4
 
-   acl_model
-   acl_resource
-   atlas_utils
-   colorize_process
-   model_processor
-   pipeline
-   tests
-   videodata
+   colorization
--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -15,9 +15,8 @@ import sys
 import mock
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../../colorization'))
-sys.path.insert(0, os.path.abspath('../../colorization/atlas_utils'))
 sys.path.insert(0, os.path.abspath('../../webservice'))
-MOCK_MODULES = ['acl']
+MOCK_MODULES = ['acl', 'acl_model', 'acl_resource', 'atlas_utils']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
@@ -33,7 +32,8 @@ author = 'Susmitha'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx_rtd_theme']
+extensions = ['sphinx.ext.napoleon', 'sphinx_rtd_theme',
+              'sphinx.ext.githubpages']
 napoleon_google_docstring = False
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -41,7 +41,8 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['atlas_utils.lib.rst', 'atlas_utils.lib.atlasutil_so']
+exclude_patterns = ['colorization.atlas_utils', 'colorization.atlas_utils.lib',
+                    'colorization.atlas_utils.lib.atlasutil.so']
 
 autodoc_default_options = {"members": True, "undoc-members": True,
                            "private-members": True,
@@ -68,4 +69,4 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
